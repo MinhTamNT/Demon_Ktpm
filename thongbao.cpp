2d3a77ce57cc270bc52cc5318491ff71d3307bc1
NguyenTheAnh-20 <2151013002anh@ou.edu.vn>
2023-10-16 21:16:13 +0700

    stt 69 chinh sua file thongbao.cpp

--- thongbao.cpp
+++ thongbao.cpp
@@ -2,5 +2,8 @@
 using namespace std:
 int main () {
 cout << "Hello Word " << endl;
+cout << "Hello World from number 69" << endl;
+cout << "Hello World from number 69" << endl;
+cout << "Hello World from number 69" << endl; 
 
 }
