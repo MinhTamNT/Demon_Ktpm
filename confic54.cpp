4169ec5141a4af58cac0a7507801f351b4572ff6
Minh Tâm <minhtam78945@gmail.com>
2023-10-23 20:50:21 +0700

    54 xoa 2 dong trong feature4-new

--- confic54.cpp
+++ confic54.cpp
@@ -3,7 +3,6 @@ using namespace std:
 int main () {
 cout << "Hello Word " << endl;
 cout << "Hello World from number 54" << endl;
-cout << "Hello World from number 54" << endl;
-cout << "Hello World from number 54" << endl; 
-return 0
+
+return 0;
 }
