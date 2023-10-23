6ea9d38f492b672a5ce7c6cf31bdf083e2fd80fd
Minh Tâm <minhtam78945@gmail.com>
2023-10-23 20:38:08 +0700

    54 da them 1 flile confic moi vao nhanh feature3-new

---
+++ confic54.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std:
+int main () {
+cout << "Hello Word " << endl;
+cout << "Hello World from number 54" << endl;
+cout << "Hello World from number 54" << endl;
+cout << "Hello World from number 54" << endl; 
+return 0
+}
